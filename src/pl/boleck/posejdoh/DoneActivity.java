c831f1119988becfc21e1aa74cdf57a46ef502e9
Pawel Mallory <Boleck95@gmail.com>
2015-02-23 22:19:51 +0100

    UPDATE new Activity

--- src/pl/boleck/posejdoh/DoneActivity.java
+++ src/pl/boleck/posejdoh/DoneActivity.java
@@ -1,39 +1,22 @@
 package pl.boleck.posejdoh;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
 
-public class DodatkiActivity extends Activity {
-
-	private TextView BtNextDA;
+public class DoneActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_dodatki);
-		BtNextDA = (TextView) findViewById(R.id.BtNextDA);
-		BtNextDA.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Przejdz do nastepnej aktywnosci
-		 		Intent intent = new Intent(
-				 		DodatkiActivity.this,
-				 		DoneActivity.class);
-				 		startActivity(intent);
-			}
-		});
+		setContentView(R.layout.activity_done);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.dodatki, menu);
+		getMenuInflater().inflate(R.menu.done, menu);
 		return true;
 	}
 
